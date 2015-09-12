52d533a94b61026ae3b801d456711599bcd773d6
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-09-12 15:36:18 +0300

    Massiivid [Delivers #103248572]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -4,7 +4,6 @@
     $Vanus = 21;
     $eesnimi_algab_vokaaliga = true;
     echo $Nimi . " " . $Perenimi . " " . "(" . $Vanus . ")";
-
     $isik = array("Evgeniy", "Kureyko", "21");
     echo $isik[0] . " " . $isik[1] . " " . "(" . $isik[2] . ")";
 ?>
