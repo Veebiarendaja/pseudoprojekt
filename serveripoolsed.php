b33978900e7842f9c14c31fe71b0a8e99c076b3e
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-09-12 15:51:50 +0300

    Tingimused [Delivers #103248578]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,9 +1,21 @@
+<!doctype html>
+<html lang="ee">
+<head>
+    <meta charset="UTF-8">
+    <title>Pseudoprojekt</title>
+</head>
+<body>
 <?php
     $Nimi = 'Evgeniy';
     $Perenimi = 'Kureyko';
     $Vanus = 21;
-    $eesnimi_algab_vokaaliga = true;
+    $eesnimi_algab_vokaaliga = true;            //Boolean
     echo $Nimi . " " . $Perenimi . " " . "(" . $Vanus . ")";
-    $isik = array("Evgeniy", "Kureyko", "21");
+    $isik = array("Evgeniy", "Kureyko", "21");                          //Array
     echo $isik[0] . " " . $isik[1] . " " . "(" . $isik[2] . ")";
+    if ($eesnimi_algab_vokaaliga = true) {         //If
+        echo "Nimi algab vokaaliga";
+    }
 ?>
+</body>
+</html>
