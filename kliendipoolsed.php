f9152a5ec714a973e37db5900d01ab732a5fe878
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-09-13 23:05:26 +0300

    Kass koeraks [Delivers #103266588]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -20,8 +20,16 @@
     }
 </script>
 
-<a href="" onclick='alert("j‰‰me siia"); event.preventDefault()'>j‰‰me siia</a> <!--Navigeerimatu link-->
+<a href="" onclick='alert("j‰‰me siia"); event.preventDefault()'>j‰‰me siia</a> <br> <!--Navigeerimatu link-->
 
+<img id="KassKoeraks" src="https://upload.wikimedia.org/wikipedia/commons/0/0a/Standing_jaguar.jpg" alt="" onclick="kasskoeraks()">
+
+<script>
+    function kasskoeraks() {
+        document.getElementById('KassKoeraks').src="http://www.citypresokc.com/wp-content/uploads/2015/03/pug.jpg";
+        src.replace()
+    }
+</script>
 
 </body>
 </html>
