dae3ce14627032a2e17608de7659f9dc46b327c3
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-09-13 16:40:16 +0300

    Navigeerimatu link [Delivers #103266428]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -6,17 +6,22 @@
 </head>
 <body>
 
-<button onclick="myFunction()">Tere Maailm!</button>
+<button onclick="myFunction()">Tere Maailm!</button> <!--Button Alert-->
+<br>
 <script>
     function myFunction() {
         alert("Tere Maailm!");
     }
 </script>
-<a  href="http://www.khk.ee" onclick='alert("Tere Maailm!")'>Tere Maailm!</a>
+<a  href="http://www.khk.ee" onclick='alert("Tere Maailm!")'>Tere Maailm!</a> <br> <!--Alert link-->
 <script>
     function linkFunction() {
         alert("Tere Maailm!");
     }
 </script>
+
+<a href="" onclick='alert("j‰‰me siia"); event.preventDefault()'>j‰‰me siia</a> <!--Navigeerimatu link-->
+
+
 </body>
 </html>
