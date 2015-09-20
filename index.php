8abc35c2742ae7c29238732821387c59d60ff2b2
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-09-20 14:51:15 +0300

    23. Require [Delivers #103743266]

--- index.php
+++ index.php
@@ -5,6 +5,20 @@
     <title>Pseudoprojekt</title>
 </head>
 <body>
-<h1>Tere Maailm!</h1>
+<?php
+$parameter = $_SERVER['QUERY_STRING'];
+$url_check = $parameter . ".php";
+if(file_exists($url_check))
+{
+   // echo $url_check;
+    echo "<script type='text/javascript'>window.alert('Ymbersuunamine lehele $url_check');</script>";
+    require __DIR__ . '/' . $url_check;
+}  else
+{
+    echo "No page found";
+}
+
+
+?>
 </body>
 </html>
