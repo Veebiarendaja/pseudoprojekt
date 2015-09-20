f1f85e9b3cd10745fcfaa075c549111a60239e7e
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-09-20 22:26:20 +0300

    24. Require refactor [Delivers #103743268]

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    $url = $_GET['page'];
+    $url_check = $url . '.php';
+    $page = (file_exists($url_check)) ? $_GET['page'] : 'home';
+?>
 <!doctype html>
 <html lang="ee">
 <head>
@@ -6,17 +11,18 @@
 </head>
 <body>
 <?php
-$parameter = $_SERVER['QUERY_STRING'];
-$url_check = $parameter . ".php";
-if(file_exists($url_check))
-{
-   // echo $url_check;
-    echo "<script type='text/javascript'>window.alert('Ymbersuunamine lehele $url_check');</script>";
-    require __DIR__ . '/' . $url_check;
-}  else
-{
-    echo "No page found";
-}
+//$parameter = $_SERVER['QUERY_STRING'];
+//$url_check = $parameter . ".php";
+require __DIR__ . '/' . $page . '.php';
+//if(file_exists($url_check))
+//{
+//   // echo $url_check;
+//    echo "<script type='text/javascript'>window.alert('Ymbersuunamine lehele $url_check');</script>";
+//    require __DIR__ . '/' . $url_check;
+//}  else
+//{
+//    echo "No page found";
+//}
 
 
 ?>
