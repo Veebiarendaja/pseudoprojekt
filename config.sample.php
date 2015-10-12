bdf0ae83f542d7afbc0f75c041eb7a904277d0e4
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-10-12 11:20:39 +0300

    Update

--- config.sample.php
+++ config.sample.php
@@ -1,7 +1,7 @@
 <?php
     define('DATABASE_USERNAME', 'root');
     define('DATABASE_HOSTNAME', 'localhost');
-//    define('DATABASE_PASSWORD', '');
-    $database_password = '';
+    define('DATABASE_PASSWORD', '');
+//    $database_password = '';
     define('DATABASE_DATABASE', 'blog');
 ?>
