c57739fcafbb51b217d2006f92e55f2b7228ae2f
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-10-28 14:51:28 +0200

    38. Tabeli sorteerimine [deliver #105980668]

--- klient.php
+++ klient.php
@@ -3,26 +3,42 @@
         border: 1px solid black;
         border-collapse: collapse;
     }
+
+    th {
+        cursor: help;
+        padding: 5px;
+        background-color: lightpink;
+    }
 </style>
 
 
-<table>
+<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.0.js"></script>
+<script type="text/javascript" src="./js/jquery.tablesorter.js"></script>
+
+<table id="myTable">
     <thead>
     <tr>
-        <th>Eesnimi</th>           <!--Table Header-->
+        <th>Eesnimi</th>        <!--Table Header-->
         <th>Perenimi</th>
         <th>Aadress</th>
     </tr>
     </thead>
+    <tbody>
+    </tbody>
 </table>
-<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.0.js">
-</script>
+
 <script>
-    $.get("api.php", function (value) {   // api.php
-        value = JSON.parse(value); //decode
-        for (var i in value) {
-            var j = value[i];
-            $('table').append("<tr><td>" + j[0] + "</td><td>" + j[1] + "</td><td>" + j[2] + "</td></tr>");
+    $(document).ready(function () {
+            $.get("api.php", function (value) {   // api.php
+                value = JSON.parse(value); //decode
+                for (var i in value) {
+                    var j = value[i];
+                    $('tbody').append("<tr><td>" + j[0] + "</td><td>" + j[1] + "</td><td>" + j[2] + "</td></tr>");
+                }
+                $("#myTable").tablesorter();
+            })
         }
-    })
+    );
+
+
 </script>
